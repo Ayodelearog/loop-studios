dfe9fac937da99d96a41889c927760b1980c61da
Ayodele <ayodelearogundade12@gmail.com>
2024-11-17 20:36:40 +0100

    fix accessibility

--- app/components/Leader.tsx
+++ app/components/Leader.tsx
@@ -13,7 +13,7 @@ function Leader() {
         </div>
 
         <div className='text-center lg:absolute lg:bottom-0 lg:bg-white lg:w-[40%] lg:right-0 lg:mr-[10%] lg:pt-[3.5rem] lg:pl-[3rem]'>
-            <h3 className='text-2xl leading-6 lg:text-left lg:text-5xl'>THE LEADER IN INTERACTIVE VR</h3>
+            <h2 className='text-2xl leading-6 lg:text-left lg:text-5xl'>THE LEADER IN INTERACTIVE VR</h2>
             <p className='mt-4 text-primary-very-dark-gray mx-2 font-semibold lg:text-left'>
                 Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning projects have been transforming business through digital experiences that bind to their brand.
             </p>
